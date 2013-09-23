a3674fcf8eeba4a42705cd05fc1f72a40366683c
Jorge Stamatio <jorge.stamatio@gmail.com>
2013-09-24 00:43:01 +0200

    fixed js

--- themes/js/js.theme.bash
+++ themes/js/js.theme.bash
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-SCM_THEME_PROMPT_PREFIX="("
-SCM_THEME_PROMPT_SUFFIX=")"
+SCM_THEME_PROMPT_DIRTY=" ${red}*${normal}"
+SCM_THEME_PROMPT_CLEAN=" ${green}✓"
+SCM_THEME_PROMPT_PREFIX=" ${yellow}["
+SCM_THEME_PROMPT_SUFFIX="${yellow}]"
 
-SCM_THEME_PROMPT_DIRTY=" ${bold_red}*${normal}"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}${normal}"
+GIT_THEME_PROMPT_DIRTY=" ${red}*${normal}"
+GIT_THEME_PROMPT_CLEAN=" ${green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${yellow}["
+GIT_THEME_PROMPT_SUFFIX="${yellow}]"
+
 SCM_GIT_CHAR="${bold_green}+${normal}"
 
 function prompt_command() {
-    PS1="${red}\u${reset_color}@${cyan}\H${yellow}:\w ${reset_color}[$(scm_prompt_info)] ${reset_color}";
+    PS1="${red}\u${reset_color}@${cyan}\H${yellow}:\w${reset_color}$(scm_prompt_info) ${reset_color}";
 }
 
 PROMPT_COMMAND=prompt_command;
