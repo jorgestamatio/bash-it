8a2e21ca2b686aee67b13d19bef0c92c4de042d0
Jorge Stamatio <jorge.stamatio@gmail.com>
2013-09-24 00:44:59 +0200

    fixed js

--- themes/js/js.theme.bash
+++ themes/js/js.theme.bash
@@ -2,18 +2,18 @@
 
 SCM_THEME_PROMPT_DIRTY=" ${red}*${normal}"
 SCM_THEME_PROMPT_CLEAN=" ${green}✓"
-SCM_THEME_PROMPT_PREFIX=" ${yellow}["
+SCM_THEME_PROMPT_PREFIX=" ${yellow}[${reset_color}"
 SCM_THEME_PROMPT_SUFFIX="${yellow}]"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}*${normal}"
 GIT_THEME_PROMPT_CLEAN=" ${green}✓"
-GIT_THEME_PROMPT_PREFIX=" ${yellow}["
+GIT_THEME_PROMPT_PREFIX=" ${yellow}[${reset_color}"
 GIT_THEME_PROMPT_SUFFIX="${yellow}]"
 
 SCM_GIT_CHAR="${bold_green}+${normal}"
 
 function prompt_command() {
-    PS1="${red}\u${reset_color}@${cyan}\H${yellow}:\w${reset_color}$(scm_prompt_info) ${reset_color}";
+    PS1="${red}\u${reset_color}@${cyan}\h${yellow}:\w${reset_color}$(scm_prompt_info) ${reset_color}";
 }
 
 PROMPT_COMMAND=prompt_command;
