81eaf024444d3407fa8312b9c38c074f8cb99fba
Jorge Stamatio <jorge.stamatio@gmail.com>
2013-09-24 00:12:14 +0200

    changed color js theme

--- themes/js/js.theme.bash
+++ themes/js/js.theme.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 function prompt_command() {
-    PS1="${red}\u@${cyan}\h ${reset_color}${white}\w${reset_color} $[scm_prompt_info] ${reset_color} ${reset_color} ${reset_color} ";
+    PS1="${red}\u${reset_color}@${cyan}\h ${reset_color}${white}\w${reset_color} $[scm_prompt_info] ${reset_color} ${reset_color} ${reset_color} ";
 }
 
 PROMPT_COMMAND=prompt_command;
